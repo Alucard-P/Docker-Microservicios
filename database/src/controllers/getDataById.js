75113bae417853102fed8ef724c44e40128a8dfb
Alucard-P <paulmct93@gmail.com>
2023-09-28 11:38:54 -0500

    database update

--- database/src/controllers/getDataById.js
+++ database/src/controllers/getDataById.js
@@ -1,13 +1,7 @@
 const store = require("../database");
-const { ClientError } = require("../utils/errors");
 
 module.exports = async (req, res) => {
   const { model, id } = req.params;
-  // const existId = await store[model].get(id);
-  // if (existId) {
   const data = await store[model].get(id);
   res.status(202).json(data);
-  // } else {
-  //   throw new ClientError("No existe el id", 404);
-  // }
 };
--- database/src/controllers/newData.js
+++ database/src/controllers/newData.js
@@ -1,7 +1,14 @@
 const store = require("../database");
+const { ClientError } = require("../utils/errors");
 
 module.exports = async (req, res) => {
   const { model } = req.params;
-  const newPost = await store[model].insert(req.body);
-  res.status(202).send({ status: "Cargado exitoso", newData: newPost });
+  const { _id } = req.body;
+  const existId = await store[model].get(_id);
+  if (!existId) {
+    const newPost = await store[model].insert(req.body);
+    res.status(202).send({ status: "Cargado exitoso", newData: newPost });
+  } else {
+    throw new ClientError("Ya existe este id, escriba otro", 404);
+  }
 };
